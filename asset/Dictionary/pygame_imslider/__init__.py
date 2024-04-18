c3f32b8345fa3dd7dad197f434c680f5e94c8ec4
Huỳnh Minh Triết <119598743+minhtrietn@users.noreply.github.com>
2024-04-18 14:16:31 +0700

    Delete asset directory

--- asset/Dictionary/pygame_imslider/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Flexible images slider highly customizable for pygame."""
-
-from .slider import ImSlider, STYPE_SLIDE, STYPE_LOOP, STYPE_FADE
-from .renderers import ImSliderRenderer
-
-__version__ = '1.0.2'
